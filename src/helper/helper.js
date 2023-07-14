f579d4cc71a59a11787565cb8eeaad3840993506
shubhanshu sahu <eddysahu31@gmail.com>
2023-07-14 19:43:34 +0530

    re

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -8,20 +8,3 @@ export const findMaxColumns = () => {
     return num > 40 ? 40 : num < 2 ? 2 : num;
 };
 
-export const generateClassName = (grid, walls, r, c) => {
-    let classNames = {cell: "", playerPos: ""};
-    let cell = grid[r][c];
-
-    Object.keys(walls).forEach((wall) => {
-    if (walls[wall]) classNames.cell+=(` border-${wall}`);
-    });
-
-    if (cell.visited) classNames.cell+=` background-purple`;
-
-    if (cell.currentPos) {
-    cell.playerVisited = true;
-    classNames.playerPos='player-position';
-    };
-
-    return classNames;
-};
